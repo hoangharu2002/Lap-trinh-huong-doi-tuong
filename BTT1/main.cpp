c675daf55720c42b7418fdf8e4bfe7595b13c389
Just4Func <nnhoang2002@gmail.com>
2023-07-23 21:10:52 +0700

    lam cau 3

--- BTT1/main.cpp
+++ BTT1/main.cpp
@@ -25,36 +25,57 @@ int getLCM(int a, int b) {
 	return a / getGCD(a, b) * b;
 }
 
+bool checkPrimeNumber(unsigned int n) {
+	for (int i = 2; i * i <= n; ++i)
+		if (n % i == 0)
+			return false;
+	return true;
+}
+
 int main() {
-	//cau 1
-	//Viet chuong trinh nhap vao hai so nguyen duong a va b. Tim uoc so chung lon nhat cua a va b.
-	{
-		int a, b;
-		cout << "Bai 1:\nNhap lan luot hai so nguyen duong: ";
-		cin >> a >> b;
-		cout << "Uoc chung lon nhat cua hai so vua nhap la: " << getGCD(a, b) << "\n\n";
-	}
-	//cau 2
-	//Viet chuong trinh nhap vao hai so nguyen duong a va b. Tim boi so chung nho nhat cua a va b.
-	{
-		int a, b;
-		cout << "Bai 2:\nNhap lan luot hai so nguyen duong: ";
-		cin >> a >> b;
-		cout << "Boi chung lon nhat cua hai so vua nhap la: " << getLCM(a, b) << "\n\n";
-	}
+	////cau 1
+	////Viet chuong trinh nhap vao hai so nguyen duong a va b. Tim uoc so chung lon nhat cua a va b.
+	//{
+	//	int a, b;
+	//	cout << "Bai 1:\nNhap lan luot hai so nguyen duong: ";
+	//	cin >> a >> b;
+	//	cout << "Uoc chung lon nhat cua hai so vua nhap la: " << getGCD(a, b) << "\n\n";
+	//}
+	////cau 2
+	////Viet chuong trinh nhap vao hai so nguyen duong a va b. Tim boi so chung nho nhat cua a va b.
+	//{
+	//	int a, b;
+	//	cout << "Bai 2:\nNhap lan luot hai so nguyen duong: ";
+	//	cin >> a >> b;
+	//	cout << "Boi chung lon nhat cua hai so vua nhap la: " << getLCM(a, b) << "\n\n";
+	//}
 	//cau 3
 	//Nhap vao so nguyen duong n. In ra man hinh cac so nguyen to la so le <= n.
+		//Cach 1
 	{
 		int n;
 		cout << "Nhap mot so nguyen duong n: ";
 		cin >> n;
-		if (n == 0)
-			cout << "Khong co so nguyen to nao <= 0";
-		else if (i == 1)
-			cout << "Khong co so nguyen to nao <= 1";
+		if (n == 0 || n == 1 || n == 2)
+			cout << "Khong co so nguyen to le nao nho hon " << n;
 		else {
-			for (int i = 2; i <= )
+			for (int i = 3; i * i <= n; ++i)
+				if (checkPrimeNumber(i))
+					cout << i << " ";
+			if (checkPrimeNumber(n))
+				cout << n << "\n\n";
 		}
+	}
+		//Cach 2
+	{
+
+	}
+	//Cau 4
+	{
+		int day, month, year;
+		cout << "Nhap ngay thang nam: ";
+		cin >> day >> month >> year;
+
 	}
 	system("pause");
 	return 0;
