9cc2629b3596100deeb93b3b308a83c84f8c544b
Just4Func <nnhoang2002@gmail.com>
2023-07-26 13:34:20 +0700

    Khoi tao chuong trinh bai tap 1

---
+++ Slide3ver1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Bai tap 1
+	//Viet chuong trinh cho phep nhap, xuat, khoi tao mot hoc sinh. Thong tin can quan tam ve mot hoc sinh gom co:
+	//Ma hoc sinh (8 ky tu);  Ho ten hoc sinh (30 ky tu); Diem toan (int); Diem van (int).
+	{
+		
+	}
+	system("pause");
+	return 0;
+}
