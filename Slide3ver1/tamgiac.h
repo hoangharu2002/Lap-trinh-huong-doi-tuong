ed12d3c4a082dad6f84aaad5aafbcd05b1d56034
Just4Func <nnhoang2002@gmail.com>
2023-07-30 23:13:22 +0700

    tao prototype constructor, destructor, nhap, xuat

---
+++ Slide3ver1/tamgiac.h
@@ -0,0 +1,28 @@
+#ifndef _TG_H_
+#define _TG_H_
+
+#include <iostream>
+#include "diem.h"
+using namespace std;
+
+class TamGiac {
+private:
+	Diem a, b, c;
+public:
+	TamGiac() {}
+	TamGiac(Diem a, Diem b, Diem c) {
+		this->a = a;
+		this->b = b;
+		this->c = c;
+	}
+	TamGiac(TamGiac& tg) {
+		this->a = tg.a;
+		this->b = tg.b;
+		this->c = tg.c;
+	}
+	~TamGiac() {}
+	static void inputTamGiac(TamGiac& tg);
+	static void printTamGiac(TamGiac& tg);
+};
+
+#endif
