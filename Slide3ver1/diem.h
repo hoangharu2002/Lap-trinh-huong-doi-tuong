4154b928da0390b99edbaebfb682930fb23c81bb
Just4Func <nnhoang2002@gmail.com>
2023-07-30 20:05:02 +0700

    tao prototype cho class Diem

---
+++ Slide3ver1/diem.h
@@ -0,0 +1,35 @@
+#ifndef _D_H_
+#define _D_H_
+
+#include <iostream>
+using namespace std;
+
+class Diem {
+private:
+	double x, y;
+public:
+	Diem() {
+		this->x = 0;
+		this->y = 0;
+	}
+	Diem(double x = 0, double y = 0) {
+		this->x = x;
+		this->y = y;
+	}
+	Diem(Diem& d) {
+		this->x = d.x;
+		this->y = d.y;
+	}
+	~Diem() {
+		this->x = 0;
+		this->y = 0;
+	}
+	void set(double x, double y);
+	double getX();
+	double getY();
+	void tinhtien(double x, double y);
+	static void inputDiem(Diem& d);
+	static void printDiem(Diem d);
+};
+
+#endif
